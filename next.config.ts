159afdd85da0a2b90527b5e16c43f40cef4424da
precious123-gifted <ogunameprecious123@gmail.com>
2025-02-10 14:57:33 +0100

    debugging

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 module.exports = {
-
-  
-
-  // ...other config
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(svg|png|jpg|gif)$/,
+      type: 'asset/resource',  // Skips Babel for large files
+    });
+    return config;
+  }
+};
